3c63790d3a18510852c0a881d0087a4c0148a35a
Nedz-space <0xnedz@gmail.com>
2025-03-21 09:06:05 +0100

    Added apps to INSTALLED_APPS and Configured REST framework in settings.py

--- tourease_backend/tourease_backend/settings.py
+++ tourease_backend/tourease_backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    'tours',
+    'accounts',
+    'bookings',
 ]
 
 MIDDLEWARE = [
@@ -98,6 +103,12 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 
 # Internationalization
