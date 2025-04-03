5c5cd87cb8f0ebcdd82ddb358a1566e5cbac02d9
Nedz-space <0xnedz@gmail.com>
2025-04-03 09:08:03 +0100

    Modified tests.py

--- tourease_backend/tours/tests.py
+++ tourease_backend/tours/tests.py
@@ -4,15 +4,22 @@
 from django.test import TestCase
 from rest_framework.test import APIClient
 from rest_framework import status
-from tours.models import Tour
+from tours.models import Tour, TourCategory
 
 class ToursTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.tour = Tour.objects.create(name="Safari Adventure", price=100.00)
+        self.category = TourCategory.objects.create(name="Adventure")
+        self.tour = Tour.objects.create(
+            title="Safari Adventure",
+            description="A thrilling safari experience",
+            location="Kenya",
+            price=100.00,
+            category=self.category
+        )
 
     def test_get_tours(self):
-        """Test retrieving list of tours"""
+        """Test retrieving a list of tours"""
         response = self.client.get('/api/tours/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
