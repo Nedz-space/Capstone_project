377ec63d8c6ce4441a363df466f1f64f6bf2bc69
Nedz-space <0xnedz@gmail.com>
2025-04-04 10:28:25 +0100

    Updated host and requirements.txt

--- tourease_backend/tourease_backend/settings.py
+++ tourease_backend/tourease_backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-tl!jj@8e=zy9@)q$hp4+90xkcdfma+)yfv-b!sjn0&l9%vb61g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.41.203.144', '*']
 
 
 # Application definition
